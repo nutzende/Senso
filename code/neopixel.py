75fe712fcc1588bf1ef4da13048c23b42e21fec9
Leon Seifert <36161194+Leon-108@users.noreply.github.com>
2025-10-30 11:58:09 +0100

    update neopixel test code

--- code/neopixel.py
+++ code/neopixel.py
@@ -8,7 +8,7 @@ class NeoPixel:
     # G R B W
     ORDER = (1, 0, 2, 3)
 
-    def __init__(self, pin, n, bpp=4, timing=1):
+    def __init__(self, pin, n, bpp=3, timing=1):
         self.pin = pin
         self.n = n
         self.bpp = bpp
