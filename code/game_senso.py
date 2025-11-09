70fc0ce5109157aa8a67545a3f379f1d32bb7ee1
Leon Seifert <36161194+Leon-108@users.noreply.github.com>
2025-11-09 21:06:57 +0100

    add button state

--- code/game_senso.py
+++ code/game_senso.py
@@ -150,6 +150,9 @@ def spiel():
     # --- Warte-Pause vor der nächsten Runde ---
     elif runde == 3:
         if time.ticks_diff(now, led_timer) >= 0:
+            while pressed > 0:
+                log("Button pressed")
+                time.sleep(0.25)
             next_round()
 
 
